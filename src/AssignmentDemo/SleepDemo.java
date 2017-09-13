95635d456ea0617d48f3569b88dce049e7dbe6ca
MithunBurman7 <mithunburman7@gmail.com>
2017-09-14 01:44:33 +0530

    14.2

---
+++ src/AssignmentDemo/SleepDemo.java
@@ -0,0 +1,14 @@
+package AssignmentDemo;
+
+public class SleepDemo {
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		ThreadSleepDemo thSl= new ThreadSleepDemo();
+		Thread thread= new Thread();
+		thread.start();
+	}
+
+}
